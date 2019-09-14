6a651cca822ca493792f303d35bd76b740dfb7de
gumball13191 <bluebeetle131@gmail.com>
2019-09-14 18:07:45 +0800

    new project

---
+++ SGCC/Assets/Scripts/CloudBehavier.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class CloudBehavier : MonoBehaviour {
+    public new Transform transform;
+    public new Camera camera;
+    public Vector3 movement;
+    private float horizontalMin;
+    private float horizontalMax;
+
+    void Start() {
+        float halfHeight = camera.orthographicSize;
+        float halfWidth = camera.aspect * halfHeight;
+
+        horizontalMin = -halfWidth;
+        horizontalMax = halfWidth;
+    }
+
+    void Update() {
+        transform.Translate(movement);
+        if (movement.x > 1 && transform.position.x > horizontalMax) {
+            transform.position.Set(horizontalMin, Random.Range(-1, 2), transform.position.z);
+        } else if (movement.x < 1 && transform.position.x < horizontalMin) {
+            transform.position.Set(horizontalMax, Random.Range(-1, 2), transform.position.z);
+        }
+    }
+}
---
+++ SGCC/Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;public class PlayerBehaviour : MonoBehaviour {
+    public new Transform transform;    public new Camera camera;    public Animator animator;    public float speed; private bool left; private bool right;    // Start is called before the first frame update
+    void Start() { }
+
+    // Update is called once per frame
+    void Update() {
+        left = Input.GetKey(KeyCode.A);
+        right = Input.GetKey(KeyCode.D);
+        if (left ^ right) {
+            if (left) transform.localRotation = Quaternion.Euler(0, 180, 0);
+            else transform.localRotation = Quaternion.Euler(0, 0, 0);
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+            animator.SetBool("walking", true);
+        } else animator.SetBool("walking", false);
+    }}
