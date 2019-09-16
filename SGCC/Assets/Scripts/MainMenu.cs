5a30e3e01471bba87bb056ec683863e662c1705b
gumball13191 <bluebeetle131@gmail.com>
2019-09-16 20:14:52 +0800

    a lot of stuff

---
+++ SGCC/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour 
+{
+    public static int socialPoints = 0;
+    public static int physicalPoints = 0;
+    public static int emotionalPoints = 0;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Buildings");
+        SceneManager.UnloadSceneAsync("menu");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
---
+++ SGCC/Assets/Scripts/ScoreMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ScoreMenu : MonoBehaviour
+{
+    public void GoScoreMenu()
+    {
+        SceneManager.LoadScene("ScoreMenu");
+    }
+    public void Continue()
+    {
+        SceneManager.UnloadSceneAsync("ScoreMenu");
+    }
+}
