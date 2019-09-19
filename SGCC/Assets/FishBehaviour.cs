daa89f96e4241f1e6fe8c36b00bd26f2e7b01826
JavierLimZH <32543880+JavierLimZH@users.noreply.github.com>
2019-09-19 20:48:58 +0800

    Fish

---
+++ SGCC/Assets/FishBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishBehaviour : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private string state = "spawned";
+    // spawned: spawning or spawned
+    // ending : closing, not clickable
+
+    private float timeState = 0;
+    Vector2 targetPos;
+    Vector2 vel;
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+
+    void OnMouseDown()
+    {
+        GameObject.Find("GameManager").GetComponent<FishGameManager>().addPoints(10);
+        Destroy(this.gameObject);
+    }
+
+    void move()
+    {
+        if (targetPos.magnitude == 0 || ((Vector2)transform.position - targetPos).magnitude < 2)
+        {
+            targetPos = GameObject.Find("GameManager").GetComponent<FishGameManager>().randomPoint()*2;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, 0.1f);
+        transform.LookAt(targetPos);
+    }
+}
---
+++ SGCC/Assets/Scripts/FishGameManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishGameManager : MonoBehaviour
+{
+    public CircleCollider2D spawnBoundary;
+    public GameObject fish;
+
+    private string gameState = "starting";
+    // starting: entered but not started
+    // started : started game, running.
+    // ended   : ended game, closing out soon.
+
+    private int points;
+    private float timeState = 30;
+    private float timeToNext = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameState == "starting") gameState = "started";
+
+        if (gameState == "ended") ;
+
+        if (timeState < 0)
+        {
+            gameState = "ended";
+            return;
+        }
+
+        if (timeToNext < 0)
+        {
+            spawnFish(randomPoint());
+            timeToNext = Random.Range(0.5f, 1);
+        }
+
+        timeToNext -= Time.deltaTime;
+        timeState -= Time.deltaTime;
+    }
+
+    public Vector2 randomPoint()
+    {
+        return spawnBoundary.transform.position + (Random.insideUnitSphere * spawnBoundary.radius);
+    }
+
+    void spawnFish(Vector2 pos)
+    {
+        Instantiate(fish, pos, Quaternion.identity);
+    }
+
+    public void addPoints(int num)
+    {
+        points += num;
+    }
+}
