efb134427c1b7f8dba4bc9e65f38917b518f252c
gumball13191 <bluebeetle131@gmail.com>
2019-09-19 19:02:42 +0800

    some bug fixxes

--- SGCC/Assets/Scripts/SocialUpdate.cs
+++ SGCC/Assets/Scripts/SocialUpdate.cs
@@ -1,18 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SocialUpdate : MonoBehaviour
 {
+    private TextMeshProUGUI textMesh;
     // Start is called before the first frame update
     void Start()
     {
-        
+        textMesh = GetComponent<TextMeshProUGUI>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<TextMesh>().text = MainMenu.physicalPoints + "";
+        textMesh.text = MainMenu.socialPoints + "";
     }
 }
