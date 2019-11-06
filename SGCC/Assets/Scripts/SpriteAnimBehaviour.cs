6ec4eaa4b8020490e62c3c611510ee61de0bb7f5
JavierLimZH <32543880+JavierLimZH@users.noreply.github.com>
2019-11-06 10:33:32 +0800

    h

---
+++ SGCC/Assets/Scripts/SpriteAnimBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAnimBehaviour : MonoBehaviour
+{
+    public double freq = 5;
+    public float scale = 0.1f;
+    Vector3 initScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = initScale + new Vector3(
+            Mathf.Sin((float)(2 * Mathf.PI * freq * Time.time)) * scale,
+            Mathf.Sin((float)(2 * Mathf.PI * freq * Time.time)) * scale,
+            0
+            );
+    }
+}
